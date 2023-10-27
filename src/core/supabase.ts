1897ddb1db5a2a8fdbacba5da97a655aee506531
vickonrails <victorofoegbu0009@gmail.com>
2023-10-27 22:40:23 +0100

    chore: switch vite with nextjs

--- src/core/supabase.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-import { Database } from "lib/database.types";
-
-export const supabase = createClient<Database>(
-    import.meta.env.VITE_SUPABASE_URL,
-    import.meta.env.VITE_SUPABASE_KEY,
-)
